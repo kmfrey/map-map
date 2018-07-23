fe70c860abc1c681eefc45c8fadc2a93b2d334e0
Kai Frey <kmfrey19@gmail.com>
2018-07-23 12:25:33 -0400

    added cross-refernce

--- country.py
+++ country.py
@@ -1,7 +1,15 @@
 from google.appengine.ext import ndb
+import pycountry
 
 class Country(ndb.Model):
-    name = ndb.StringProperty(required=True)
-    policies = ndb.StringProperty(required=True, repeated=True)
-    culture = ndb.StringProperty(required=True, repeated=True)
-    english = ndb.BooleanProperty(required=True)
+    def get(self):
+        name = ndb.StringProperty(required=True)
+        for i in range (0, 250):
+        if pycountry.countries.name == name:
+            index = pycountry.countries.name[i]
+        else:
+            self.response.write("Country does not exist")
+        policies = ndb.StringProperty(required=True, repeated=True)
+        culture = ndb.StringProperty(required=True, repeated=True)
+        english = ndb.BooleanProperty(required=True)
+        currency = pycountry.currency.name[index]
