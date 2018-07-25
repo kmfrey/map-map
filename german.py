a644a04049d9e07cfce6294cd8fb30a05ffebc7c
Kai Frey <kmfrey19@gmail.com>
2018-07-25 17:50:10 -0400

    added lib

--- german.py
+++ german.py
@@ -10,7 +10,7 @@ r = requests.get("https://www.reisewarnung.net/api?country=US" # +alpha_2
 
 json = json.loads(r.text)
 
-rating = (json["data"]["situation"]["rating"])
+rating = json["data"]["situation"]["rating"]
 warning = json["data"]["lang"]["en"]["advice"]
 learn_more = json["data"]["lang"]["en"]["url_details"]
 
@@ -19,7 +19,7 @@ dolla = pycountry.currencies.get(name = "US Dollar")
 print (dolla)
 #change country name into alpha_2 for this, alpha_3 for currency
 
-# r_c = requests.get("http://data.fixer.io/api/latest?access_key=f6857a4dc14c06a10a11b4acccd1ddec&base%20=USD")
+r_c = requests.get("http://data.fixer.io/api/latest?access_key=f6857a4dc14c06a10a11b4acccd1ddec&base%20=USD")
 # json_2 = json.loads(r_c.text)
 #
 # print (json_2)
