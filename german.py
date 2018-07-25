c7b54f45d746233e2a1e3d066c90957388944248
Kai Frey <kmfrey19@gmail.com>
2018-07-25 12:55:04 -0400

    map to globe

--- german.py
+++ german.py
@@ -14,7 +14,7 @@ dolla = pycountry.currencies.get(name = "US Dollar")
 print (dolla)
 #change country name into alpha_2 for this, alpha_3 for currency
 
-# r_c = requests.get("http://data.fixer.io/api/f6857a4dc14c06a10a11b4acccd1ddec")
+# r_c = requests.get("http://data.fixer.io/api/latest?access_key=f6857a4dc14c06a10a11b4acccd1ddec&base%20=USD")
 # json_2 = json.loads(r_c.text)
 #
-# print (json[""])
+# print (json_2)
